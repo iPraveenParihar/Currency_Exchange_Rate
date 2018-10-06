2a2184e39615b82a699ae29fb200cde65cdf94b9
Praveen <30765265+iPraveenParihar@users.noreply.github.com>
2018-10-06 20:27:28 +0530

    Update check_internet.py

--- check_internet.py
+++ check_internet.py
@@ -2,14 +2,28 @@ import json
 import os
 import time
 
-def read():
+def curr_list():
 
+	file = open("demo_file.json","r").read()
+	
+	parse = json.loads(file)
+	print("\n")
+	print(file)
+	
+	
+def read():
+	print("\n")
 	file = open("data.json","r")
 	t = time.ctime(os.path.getmtime("data.json"))
-	print("Time:",t)
 	data = file.read()
 	parse = json.loads(data)
 	rate = parse["rates"]["INR"]
+	disclaimer = parse["disclaimer"]
+	print(disclaimer)
+	print("Time:",t)
 	print(rate)
+	print("\n")
+	curr_list()
+
 
 read()
